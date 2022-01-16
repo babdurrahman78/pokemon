7917f374fbc9e7d197279baa8238b1e7f13061d6
Vika Putri Ariyanti <vikaputriariyanti@gmail.com>
2022-01-16 18:50:25 +0700

    update

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,12 +4,13 @@ const Navig = () => {
   return (
     <nav className="navbar navbar-expand-sm bg-light navbar-light px-3">
       <a className="navbar-brand" href="/">Home</a>
+
       <div className="ml-auto">
         <a className="navbar-brand d-sm-none" href="/mypokemon">My Pokemon</a>
       </div>
 
       <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-          <a className="navbar-brand d-none d-sm-block" href="/mypokemon">My Pokemon</a>
+        <a className="navbar-brand d-none d-sm-block" href="/mypokemon">My Pokemon</a>
       </div>
 
     </nav>
